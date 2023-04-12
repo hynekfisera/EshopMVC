e7882c984a7ec7b53b3bd3874041236abf05caf0
Hynek Fišera <hynek.fisera@seznam.cz>
2023-04-12 17:20:47 +0200

    Browse products in categories

--- EshopMVC/Controllers/HomeController.cs
+++ EshopMVC/Controllers/HomeController.cs
@@ -32,9 +32,22 @@ namespace EshopMVC.Controllers
 			var kategorie = _context.Kategorie.Include(x => x.ChildKategorie).Include(x => x.ParentKategorie).Where(x => x.KategorieId == new Guid(id)).ToList()[0];
 			if (kategorie != null)
 			{
-				return View(kategorie);
+				var produkty = ZiskejProdukty(kategorie);
+				return View((kategorie, produkty));
 			}
 			return View();
+		}
+
+		public List<Vyrobek> ZiskejProdukty(Kategorie kategorie)
+		{
+			var list = new List<Vyrobek>();
+			list.AddRange(_context.Vyrobek.Where(x => x.KategorieId == kategorie.KategorieId).ToList());
+			var updatedKategorie = _context.Kategorie.Where(x => x.KategorieId == kategorie.KategorieId).Include(x => x.ChildKategorie).ToList()[0];
+			foreach (var childKategorie in updatedKategorie.ChildKategorie)
+            {
+				list.AddRange(ZiskejProdukty(childKategorie));
+            }
+            return list;
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
